13fd1acbf1ae89e45d1dc6c4a96341e85b92a834
ubuntu <ubuntu@ubuntu>
2017-10-31 09:20:19 +0100

    Stage show comentado.2

--- src/libros/gui/controller/RegistroController.java
+++ src/libros/gui/controller/RegistroController.java
@@ -5,7 +5,6 @@
  */
 package libros.gui.controller;
 
-import java.awt.Checkbox;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
@@ -13,12 +12,17 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 import javafx.stage.WindowEvent;
+import javafx.scene.control.CheckBox;
+
+
 
 /**
  * FXML Controller class
@@ -52,7 +56,7 @@ public class RegistroController implements Initializable {
     @FXML
     private Button btnRegistrar;
     @FXML
-    private Checkbox aceptarTerminos;
+    private CheckBox aceptarTerminos;
 
     /**
      * Initializes the controller class.
@@ -64,24 +68,49 @@ public class RegistroController implements Initializable {
 
     @FXML
     private void validarDatos(ActionEvent event) {
-        if(!textUser.getText().equals("u")){
-               if(textPass.getText().equals(TextPass2)){
-                    textNombre.setVisible(true);
-                    textApellido1.setVisible(true);
-                    textApellido2.setVisible(true);
-                    textDireccion.setVisible(true);
-                    textTelefono.setVisible(true);
-                    textEmail.setVisible(true);
-                    textCuenta.setVisible(true);
-                    btnRegistrar.setVisible(true);
-                    aceptarTerminos.setVisible(true);
-               }
+        if(textUser.getText().trim().equals("")||textPass.getText().trim().equals("")||TextPass2.getText().trim().equals("")){
+            Alert alert = new Alert(AlertType.ERROR, "Rellene todos los campos");
+            alert.showAndWait();
+        }else{
+            if(!textUser.getText().equals("u")){
+                if(textPass.getText().trim().equals(TextPass2.getText().trim())){
+                      textNombre.setDisable(false);
+                      textApellido1.setDisable(false);
+                      textApellido2.setDisable(false);
+                      textDireccion.setDisable(false);
+                      textTelefono.setDisable(false);
+                      textEmail.setDisable(false);
+                      textCuenta.setDisable(false);
+                      btnRegistrar.setDisable(false);
+                      aceptarTerminos.setDisable(false);
+                      
+                      textUser.setDisable(true);
+                      textPass.setDisable(true);
+                      TextPass2.setDisable(true);
+                }else{
+                      Alert alert = new Alert(AlertType.ERROR, "Las contraseñas deben coincidir");
+                      alert.showAndWait(); 
+                } 
+            }else{
+               Alert alert = new Alert(AlertType.ERROR, "Nombre de usuario en uso");
+               alert.showAndWait(); 
+            }
+        }      
     }
-        
   
 
     @FXML
-    private void registrarse(ActionEvent event) {   
+    public void registrarse(ActionEvent event) {   
+        if(textNombre.getText().trim().equals("")||textApellido1.getText().trim().equals("")||textApellido2.getText().trim().equals("")||
+                textEmail.getText().trim().equals("")|| textDireccion.getText().trim().equals("")||textTelefono.getText().trim().equals("")
+                ||textCuenta.getText().trim().equals("")){
+             Alert alert = new Alert(AlertType.ERROR, "Rellene todos los campos");
+             alert.showAndWait();
+        }else{
+            
+        }
+        
+        
     }
     
     
@@ -99,15 +128,15 @@ public class RegistroController implements Initializable {
     
     public void handleWindowShowing(WindowEvent event){
            //se ejecuta antes de iniciar la ventana
-           textNombre.setVisible(false);
-           textApellido1.setVisible(false);
-           textApellido2.setVisible(false);
-           textDireccion.setVisible(false);
-           textTelefono.setVisible(false);
-           textEmail.setVisible(false);
-           textCuenta.setVisible(false);
-           btnRegistrar.setVisible(false);
-           aceptarTerminos.setVisible(false);
+           textNombre.setDisable(true);
+           textApellido1.setDisable(true);
+           textApellido2.setDisable(true);
+           textDireccion.setDisable(true);
+           textTelefono.setDisable(true);
+           textEmail.setDisable(true);
+           textCuenta.setDisable(true);
+           btnRegistrar.setDisable(true);
+           aceptarTerminos.setDisable(true);
     } 
     
 }
